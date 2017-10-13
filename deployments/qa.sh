7dcef8bc3058e9f9f898ad126a18bb50af6b60c8
Arturo Cobos <arturo@fronterait.xyz>
2017-10-13 11:26:30 -0500

    --token parameter

--- deployments/qa.sh
+++ deployments/qa.sh
@@ -1,5 +1,5 @@
 npm install -g now
 echo "deploying"
-URL=$(now --docker --public -t $NOW_TOKEN)
+URL=$(now --docker --public --token $NOW_TOKEN)
 echo "running acceptance on $URL"
 curl --silent -L $URL
